92c45ce72fc5c44807817938ec4ea59683daf05b
casp122b@easv365.dk <casp122b@easv365.dk>
2017-11-28 11:30:29 +0100

    all images is now located in the assets folder.

--- src/app/front-page/front-page-detail/front-page-detail.component.ts
+++ src/app/front-page/front-page-detail/front-page-detail.component.ts
@@ -12,10 +12,10 @@ export class FrontPageDetailComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-    this.rooms = [{src:"https://image.slidesharecdn.com/accommodationpresentationontypesofroom1-140222114118-phpapp01/95/typesofrooms-in-hotelashish-jumadeihm-ahmedabad-3-638.jpg?cb=1393069370",
+    this.rooms = [{src:"../../../assets/images/typesofrooms-in-hotelashish-jumadeihm-ahmedabad-3-638.jpg",
     routerLink:"/singleRooms/"}, 
-    {src:"http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/images/amexpub/0043/3510/201406-w-top-rated-hotel-beds-in-america-distrikt-hotel.jpg?itok=6J8ig1RL",
-    routerLink:"/doubleRooms/"}, {src:"https://ritzcarlton-h.assetsadobe.com/is/image/content/dam/the-ritz-carlton/hotels/usa-and-canada/washington-dc/washington-d-c-/guest-rooms/The%20Ritz-Carlton%20DC%201BDR%20Suite%20Bedroom.png?$XlargeViewport100pct$",
+    {src:"../../../assets/images/201406-w-top-rated-hotel-beds-in-america-distrikt-hotel.jpg",
+    routerLink:"/doubleRooms/"}, {src:"../../../assets/images/The Ritz-Carlton-DC-1BDR-Suite-Bedroom.jpg",
     routerLink:"/suites/"
     }]
   }
