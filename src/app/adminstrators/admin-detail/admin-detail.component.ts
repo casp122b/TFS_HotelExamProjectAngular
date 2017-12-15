132d6c49d0491f45ba695565db917f2a48d0d090
casp122b@easv365.dk <casp122b@easv365.dk>
2017-12-15 14:04:34 +0100

    userid

--- src/app/adminstrators/admin-detail/admin-detail.component.ts
+++ src/app/adminstrators/admin-detail/admin-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AdminService } from '../shared/admin.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Admin } from '../shared/admin.model';
 
 @Component({
   selector: 'app-admin-detail',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-detail.component.css']
 })
 export class AdminDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  
+    adminId: number;
+    newAdminGroup: FormGroup;
+    constructor(private adminService: AdminService,
+                private fb: FormBuilder,
+                private router: Router,
+                private route: ActivatedRoute) {
+        this.newAdminGroup = this.fb.group({
+          firstName: '',
+          lastName: '',
+          address: '',
+          userId: ''
+        })
+     }
+  
+     ngOnInit() {
+      this.route.paramMap
+              .switchMap(params =>
+                this.adminService.getById(+params.get('id'))
+             ).subscribe(adminId => this.adminId = adminId.id);
+    }
+  
+    editAdmin() {
+      const currentAdmin = this.adminId;
+      const newValues = this.newAdminGroup.value;
+      const updatedAdmin: Admin = {
+        id: currentAdmin,
+        firstName: newValues.firstName,
+        lastName: newValues.lastName,
+        address: newValues.address,
+        userId: newValues.userId
+      };
+      this.adminService.update(currentAdmin, updatedAdmin)
+      .subscribe(admin => {
+        this.newAdminGroup.reset();
+        this.router.navigateByUrl("/front")
+      });
+    }
 }
