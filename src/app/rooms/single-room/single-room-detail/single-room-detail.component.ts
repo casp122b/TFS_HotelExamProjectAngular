2e60116d39505f54cc07399de46943d2a25f8402
casp122b@easv365.dk <casp122b@easv365.dk>
2017-12-29 22:08:56 +0100

    Revert "Revert "y""
    This reverts commit 5e3ee62439c75c0351290ddd537bd1f1abadcbfc.

--- src/app/rooms/single-room/single-room-detail/single-room-detail.component.ts
+++ src/app/rooms/single-room/single-room-detail/single-room-detail.component.ts
@@ -3,6 +3,14 @@ import { SingleRoomService } from '../shared/single-room.service';
 import { SingleRoom } from '../shared/single-room.model';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { BookingService } from '../../../bookings/booking.service';
+import { Booking } from '../../../bookings/booking.model';
+import { GuestService } from '../../../guests/shared/guest.service';
+import { Guest } from '../../../guests/shared/guest.model';
+import { forEach } from '@angular/router/src/utils/collection';
+
+const now = new Date();
 
 @Component({
   selector: 'app-single-room-detail',
@@ -11,16 +19,27 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class SingleRoomDetailComponent implements OnInit {
 
+  guestId: number = JSON.parse(localStorage.getItem('guestId'));
+  guests: Guest[];
+  guest: Guest;
+  userId: number = JSON.parse(localStorage.getItem('userId'));
   singleRoomId: number;
   newSingleRoomGroup: FormGroup;
+  createBookingGroup: FormGroup;
   constructor(private singleRoomService: SingleRoomService,
     private fb: FormBuilder,
     private router: Router,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private bookingService: BookingService,
+    private guestService: GuestService) {
     this.newSingleRoomGroup = this.fb.group({
       price: '',
       available: '',
       name: ''
+    });
+    this.createBookingGroup = this.fb.group({
+      checkIn: null,
+      checkOut: null
     });
   }
   //The params of the route is taken and used to find the corresponding singleRoom. singleRoom.id is grabbed,and put into local variables for later use.
@@ -44,6 +63,39 @@ export class SingleRoomDetailComponent implements OnInit {
       .subscribe(singleRoom => {
         this.newSingleRoomGroup.reset();
         this.router.navigateByUrl("/front");
+      });
+  }
+
+  model: NgbDateStruct;
+  model2: NgbDateStruct;
+
+  date: { year: number, month: number };
+
+  createBooking() {
+    const thisSingleRoomId = this.singleRoomId;
+    const bookingValues = this.createBookingGroup.value;
+
+    const setYear = this.model.year;
+    const setMonth = this.model.month;
+    const setDay = this.model.day;
+    const mod1 = new Date(setYear,setMonth,setDay);
+    const setYear2 = this.model2.year;
+    const setMonth2 = this.model2.month;
+    const setDay2 = this.model2.day;
+    const mod2 = new Date(setYear2,setMonth2,setDay2);
+    //const testMod = JSON.parse(this.model.year + '-' + this.model.month + '-' + this.model.day);
+    //const anotherTest = new Date(testMod);
+    //console.log(anotherTest);
+    //console.log(this.model.year + '-' + this.model.month + '-' + this.model.day)
+    const newBooking: Booking = {
+      checkIn: mod1,
+      checkOut: mod2,
+      guestId: this.guestId,
+      singleRoomId: thisSingleRoomId
+    };
+    this.bookingService.create(newBooking)
+      .subscribe(booking => {
+        this.router.navigateByUrl('/front');
       });
   }
 }
